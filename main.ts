ad380416808b520bf68e9c4c3f7c99fdc7a14d12
Zaiba <154451069+zaiba1415@users.noreply.github.com>
2024-02-28 03:38:14 -0800

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,15 @@
+import inquirer from "inquirer";
+async function todolist(){
+    let {task}= await inquirer.prompt([{
+        type:"input",
+        name:"task",
+        message:"What would you like to add?"
+    }]);
+    console.log(`Added task: ${task}`);
+    
+}
+todolist()
+
+
+
+
